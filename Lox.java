12005acf75a8fc5d08e4c9d3910d165a8e89e30a
joel <joel@localhost.localdomain>
2024-07-09 20:53:22 +0530

    Lexical Analyzer

---
+++ Lox.java
@@ -0,0 +1,68 @@
+package interpreter.lox;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.List;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import interpreter.lox.Token;
+import interpreter.lox.Scanner;
+
+public class Lox{
+    static boolean hadError = false;
+    public static void main(String[] args) throws IOException{
+
+        if(args.length > 1){
+            System.out.println("Usage: jlox[Script]");
+            System.exit(12);
+        }
+        else if(args.length == 1){
+            runFile(args[0]);
+        }
+        else{
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException{
+        byte bytes[] = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if(hadError){
+            System.exit(20);
+        }
+    }
+
+    private static void runPrompt() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(;;){
+            System.out.print(">");
+            String input = br.readLine();
+            if(input == null){
+                break;
+            }
+            run(input);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source){
+         Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String msg){
+        report(line," ",msg);
+    }
+
+    private static void report(int line,String where,String msg){
+        System.err.println("[line " + line +"] Error"+ where + ": "+msg);
+        hadError = true;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Toy-Interpreter-Java
+
+It's based on the principles and techniques taught in the book "Crafting Interpreters" by Bob Nystrom.
+
+## Features
+
+- **Lexical Analysis**: Tokenizes raw source code into a stream of tokens.
+- **Parsing**: Converts the tokens into an Abstract Syntax Tree (AST).
+- **Interpretation**: Executes the AST directly.
+
+## Getting Started
+
+### Prerequisites
+
+- Java Development Kit (JDK) 8 or above
+
+### Installation
+
+1. Clone the repository
+  git clone https://github.com/joelb101/toy-interpreter-java.git
+
+2. Navigate to the project directory
+   cd toy-interpreter
+
+3. Compile the Java files
+   javac *.jav
+
+### Usage
+
+To run your code using the interpreter, use the following command:
+   java Main yourfile.lox
+Replace `yourfile.lox` with the path to the `.lox` file you want to interpret.
+
+## Acknowledgements
+
+- Bob Nystrom, for his excellent book "Crafting Interpreters"
